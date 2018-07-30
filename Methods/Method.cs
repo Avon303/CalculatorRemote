74780b91966f590a1f887ec9fb71d5adde44c773
Avon303 <avon.barkstdale@gmail.com>
2018-07-30 23:00:29 +0100

    Methods class library

---
+++ Methods/Method.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Method
+    {
+        //Add your methods here...
+    }
+}
