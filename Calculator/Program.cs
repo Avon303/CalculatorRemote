2972c25ca2a178c17cb2393f5e402b28710596de
Avon303 <avon.barkstdale@gmail.com>
2018-07-30 22:26:02 +0100

    initial

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void printNumber(int number)
+        {
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
